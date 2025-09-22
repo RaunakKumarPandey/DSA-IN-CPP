a9d002f4a1496909af0e14262dee7119179a541f
RaunakKumarPandey <raunakniraj2000@gmail.com>
2025-09-22 19:30:23 +0530

    some code

--- 8. Recursion/fibonacci.cpp
+++ 8. Recursion/fibonacci.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
 using namespace std;
+int fibo(int n){
+    if(n==1||n==2) return 1;
+    return fibo(n-1) + fibo(n-2);
+}
 int main(){
-    
+    // 1 1 2 3 5 8 13 21 34 55 89...
+    int n;
+    cout<<"Enter n :";
+    cin>>n;
+    cout<<fibo(n);
 }
